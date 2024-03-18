f64869b44d0cf499292f2fc1f603029f95bd5484
tto <myatthuta2019@gamil.com>
2024-03-18 22:02:21 +0630

    exerceise

---
+++ Test/src/Exercise.java
@@ -0,0 +1,16 @@
+
+public class Exercise {
+
+	public static void main(String[] args) {
+//		String firstname = "Mg Mg";
+//		String lastname = " Oo";
+//		String fullname = firstname+lastname;
+//		System.out.println(fullname);
+		
+		int x = 10;
+		int y = 15;
+		System.out.println(x+y);
+
+	}
+
+}
